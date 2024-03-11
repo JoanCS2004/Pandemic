cc0d06bcd36ba62ad7422c92ff718d550e446d55
JoanCS <75041359+JoanCS2004@users.noreply.github.com>
2024-03-11 17:52:06 +0100

    Add files via upload

--- DatosYCiudades.java
+++ DatosYCiudades.java
@@ -6,16 +6,16 @@ import java.util.*;
 public class DatosYCiudades {
 
 	public static void main(String[] args) {
-		String []ciudad = new String[10];
-		String []coordenadas = new String[10];
-		String[] ciudadesCoolindantes = new String[10];
-		String[] subCiudades = new String[20];
-		String[] distancia = new String[10];
-		int [] distanciaNueva = new int[10];
-		String texto[] = new String[20];
+		String []ciudad = new String[50];
+		String []coordenadas = new String[50];
+		String[] ciudadesCoolindantes = new String[50];
+		String[] subCiudades = new String[50];
+		String[] distancia = new String[50];
+		int [] distanciaNueva = new int[50];
+		String texto[] = new String[50];
 		int i =0;
-		int coordenada1;
-		int coordenada2;
+		int []coordenada1 = new int [50];
+		int []coordenada2 = new int [50];
 		
 		FileReader fr = null;
 		BufferedReader br;
@@ -27,9 +27,6 @@ public class DatosYCiudades {
 			// Lectura del fichero
 			String linea;
 			while((linea = br.readLine())!= null) {
-				coordenada1 = 0;
-				coordenada2 = 0;
-				
 				texto = linea.split(";");
 				ciudad[i] = texto[0];
 				coordenadas[i] = texto[2];
@@ -37,20 +34,18 @@ public class DatosYCiudades {
 				subCiudades = ciudadesCoolindantes[i].split(",");
 				
 				//Calcular la distancia
-				distancia = coordenadas[i].split(",",2);
-				coordenada1 = Integer.parseInt(distancia[i]); // Convertir se string a int
-				coordenada2 = Integer.parseInt(distancia[i]); // Convertir se string a int
-				distanciaNueva[i] = coordenada1 + coordenada2;
-				
+				distancia = coordenadas[i].split(",");
+				coordenada1[i] = Integer.parseInt(distancia[i]); // Convertir de string a int
+				coordenada2[i] = Integer.parseInt(distancia[i]); // Convertir de string a int
 				System.out.println("La ciudad " + ciudad[i] + " está en las coordenadas " +  coordenadas[i] + " sus ciudades colindantes son: ");
 				
-				for(i=0; i<distanciaNueva.length; i++) {
+				for(i=0; i<distancia.length; i++) {
 					System.out.println(subCiudades[i] + ", que está a una distancia de " + distanciaNueva[i]);
 				}
 			}
 			 
 		}catch (IOException e) {
-	        e.printStackTrace(System.out);
+	        System.out.println("error");;
 	    }	
 	}
 
